376740d5136e485055a080447dc733cf07b972f9
wangyong <wangyong@cmsr.chinamobile.com>
2023-05-08 17:23:22 +0800

    定义软连接

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -1,15 +1,11 @@
 import { computed } from 'vue'
 import { PC_DEVICE_WIDTH } from '@/constants'
 import { useWindowSize } from '@vueuse/core'
-const { width } = useWindowSize()
+const { width } = useWindowSize() // 轻松地获取并响应浏览器窗口的变化
 
 /**
  * 判断当前是否为移动设备
  */
 export const isMobileTerminal = computed(() => {
-  // 方式1：
-  // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-  //   navigator.userAgent
-  // )
   return width.value < PC_DEVICE_WIDTH
 })
