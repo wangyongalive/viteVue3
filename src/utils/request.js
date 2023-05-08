2c6e05a5333feb127f1591da199bbeff3c7daeee
wangyongalive <jsonwang0102@163.com>
2023-05-09 00:00:42 +0800

    响应拦截器

--- src/utils/request.js
+++ src/utils/request.js
@@ -17,7 +17,16 @@ service.interceptors.request.use(
 
 // 响应拦截器
 service.interceptors.response.use(
-  (response) => {},
+  (response) => {
+    const { success, message, data } = response.data
+    //   要根据success的成功与否决定下面的操作
+    if (success) {
+      return data
+    } else {
+      // TODO：业务错误
+      return Promise.reject(new Error(message))
+    }
+  },
   (error) => {}
 )
 
