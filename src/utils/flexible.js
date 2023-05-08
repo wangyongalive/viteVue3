c9d9f051ca8a3cf2dced324f2a836396ab34a1bb
wangyongalive <jsonwang0102@163.com>
2023-05-08 23:11:22 +0800

    vueUse响应式获取屏幕宽度

--- src/utils/flexible.js
+++ src/utils/flexible.js
@@ -7,9 +7,5 @@ const { width } = useWindowSize()
  * 判断当前是否为移动设备
  */
 export const isMobileTerminal = computed(() => {
-  // 方式1：
-  // return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-  //   navigator.userAgent
-  // )
   return width.value < PC_DEVICE_WIDTH
 })
