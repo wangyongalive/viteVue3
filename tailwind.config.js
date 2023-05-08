aee4cd5b1c62cf59e247e02d7eec92634b9f1cda
wangyong <wangyong@cmsr.chinamobile.com>
2023-05-08 15:11:06 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  // tailwind应用
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
