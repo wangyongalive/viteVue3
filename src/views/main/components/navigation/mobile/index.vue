b98beb79f4c8bf689cae110b755abed4dd0512f9
wangyongalive <jsonwang0102@163.com>
2023-05-09 21:26:19 +0800

    通用组件：基于vite的统一svg处理svg-icon

--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -3,13 +3,20 @@
    距离顶部或底部一定距离的位置，当页面滚动到该位置时，
   元素会停留在原地，不再随页面滚动而移动，直到页面滚动到另一个位置。 -->
   <div class="bg-white sticky top-0 left-0 z-10">
+    <!-- 汉堡按钮  shadow-l-white 白色阴影效果-->
+    <li
+      class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
+    >
+      <m-svg-icon class="w-1.5 h-1.5" name="hamburger"></m-svg-icon>
+    </li>
     <ul
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
     >
+      <!-- last:mr-4 最后一个元素 margin-right -->
       <li
         v-for="item in data"
         :key="item.id"
-        class="shrink-0 px-1.5 py-0.5 z-10 duration-200"
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
       >
         {{ item.name }}
       </li>
