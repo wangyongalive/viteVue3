41a32677e920670dbb3645441c51f28fc9a1ee99
wangyongalive <jsonwang0102@163.com>
2023-05-09 21:04:18 +0800

    动态rem基准，修正tailwind

--- src/main.js
+++ src/main.js
@@ -2,5 +2,9 @@ import { createApp } from 'vue'
 import router from './router'
 import './styles/index.scss'
 import App from './App.vue'
+import { useREM } from './utils/flexible'
+
+// 设置 rem
+useREM()
 
 createApp(App).use(router).mount('#app')
