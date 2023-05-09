c126b53eef7796fb6847b9913d0fb815369f8ab5
wangyongalive <jsonwang0102@163.com>
2023-05-09 21:36:33 +0800

    vite处理svg矢量图

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import { createApp } from 'vue'
 import router from './router'
 import './styles/index.scss'
 import App from './App.vue'
+import mLibs from './libs'
 import { useREM } from './utils/flexible'
+// 注册svg-icon
+import 'virtual:svg-icons-register'
 
 // 设置 rem
 useREM()
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).use(mLibs).mount('#app')
