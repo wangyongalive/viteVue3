4fd9e40ef9a731face38825f82b1e104c81c8c1f
wangyongalive <jsonwang0102@163.com>
2023-05-09 20:52:44 +0800

    业务组件：移动端navagationBar

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <mobile-navigation-vue v-if="isMobileTerminal" />
+  <mobile-navigation-vue v-if="isMobileTerminal" :data="categoryData" />
   <pc-navigation-vue v-else></pc-navigation-vue>
 </template>
 
@@ -7,6 +7,13 @@
 import { isMobileTerminal } from '@/utils/flexible'
 import mobileNavigationVue from './mobile/index.vue'
 import pcNavigationVue from './pc/index.vue'
+import { getCategory } from '@/api/category'
+
+const categoryData = ref([])
+const getCategoryData = async () => {
+  const { categorys } = await getCategory()
+  categoryData.value = categorys
+}
 </script>
 
 <style lang="scss" scoped></style>
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -1,7 +1,29 @@
 <template>
-  <div>移动端navigation</div>
+  <!-- position: sticky 是一个 CSS 定位属性，用于将元素固定在
+   距离顶部或底部一定距离的位置，当页面滚动到该位置时，
+  元素会停留在原地，不再随页面滚动而移动，直到页面滚动到另一个位置。 -->
+  <div class="bg-white sticky top-0 left-0 z-10">
+    <ul
+      class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+    >
+      <li
+        v-for="item in data"
+        :key="item.id"
+        class="shrink-0 px-1.5 py-0.5 z-10 duration-200"
+      >
+        {{ item.name }}
+      </li>
+    </ul>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+defineProps({
+  data: {
+    type: Array,
+    required: true
+  }
+})
+</script>
 
 <style lang="scss" scoped></style>
