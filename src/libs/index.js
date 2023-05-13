c55d28457e9fc935207195de250352d4cb4bb26f
wangyongalive <jsonwang0102@163.com>
2023-05-13 22:05:22 +0800

    组件自动化注册

--- src/libs/index.js
+++ src/libs/index.js
@@ -1,10 +1,13 @@
+// defineAsyncComponent  异步组件
 import { defineAsyncComponent } from 'vue'
 
 export default {
   // install 方法
   install(app) {
     // 获取当前路径任意文件夹下的 index.vue 文件
+    // glob 从文件系统导入多个模块
     const components = import.meta.glob('./*/index.vue')
+    console.log(components)
     // 遍历获取到的组件模块
     for (const [key, value] of Object.entries(components)) {
       // 拼接组件注册的 name
