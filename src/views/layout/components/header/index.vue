f16642ff69deb3cbafef077ef8376387b752f99a
wangyongalive <jsonwang0102@163.com>
2023-05-14 00:51:45 +0800

    header模块处理分析与简单实现

--- src/views/layout/components/header/index.vue
+++ src/views/layout/components/header/index.vue
@@ -1,10 +1,25 @@
 <template>
-    <div>
-        headering
+    <div class="w-full bg-white border-b  border-b-zinc-200 px-2 py-1">
+        <div class="flex items-center">
+            <img v-lazy class="h-4 cursor-pointer mr-2 guide-home"
+                src="https://m.imooc.com/static/wap/static/common/img/logo-small@2x.png" @click="onToHome" />
+            <header-search-vue class="mr-1"></header-search-vue>
+            <header-theme-vue class="mr-1"></header-theme-vue>
+            <header-my-vue></header-my-vue>
+        </div>
     </div>
 </template>
 
-<script setup>
+<script setup name="header">
+import { useRouter } from 'vue-router'
+import headerSearchVue from './header-search/index.vue'
+import headerThemeVue from './header-theme.vue'
+import headerMyVue from './header-my.vue'
+
+const router = useRouter()
+const onToHome = () => {
+    router.push('/')
+}
 
 </script>
 
