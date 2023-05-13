1a97678a5d28f65140fa183cf5b9c74f2c4b5f6f
wangyongalive <jsonwang0102@163.com>
2023-05-13 15:49:45 +0800

    补全category数据

--- src/views/main/components/navigation/index.vue
+++ src/views/main/components/navigation/index.vue
@@ -4,17 +4,20 @@
 </template>
 
 <script setup>
-import {ref} from 'vue'
+import { ref } from 'vue'
 import { isMobileTerminal } from '@/utils/flexible'
 import mobileNavigationVue from './mobile/index.vue'
 import pcNavigationVue from './pc/index.vue'
 import { getCategory } from '@/api/category'
+import { ALL_CATEGORY_ITEM } from '@/constants'
+
 
 // 获取navigationBar的数据
 const categoryData = ref([])
 const getCategoryData = async () => {
   const { categorys } = await getCategory()
   categoryData.value = categorys
+  categoryData.value.unshift(ALL_CATEGORY_ITEM)
 }
 getCategoryData()
 </script>
