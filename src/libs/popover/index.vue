506a2d8c58d75f51d98774432ca7e82906306b97
wangyongalive <jsonwang0102@163.com>
2023-05-22 23:07:19 +0800

    气泡功能基本功能实现

--- src/libs/popover/index.vue
+++ src/libs/popover/index.vue
@@ -4,32 +4,38 @@
       <!-- 具名插槽  触发弹出层的视图-->
       <slot name="reference" />
     </div>
+
     <!-- 气泡展示动画 -->
     <transition name="slide">
-      <!-- 匿名插槽 弹出层视图中需要展示的内容-->
-      <slot />
+      <div v-show="isVisable" class="absolute p-1 z-20 bg-white border rounded-md">
+        <!-- 匿名插槽 弹出层视图中需要展示的内容-->
+        <slot />
+      </div>
     </transition>
   </div>
 </template>
-<script>
-
-</script>
+<script></script>
 
 <script setup>
+import { ref } from 'vue'
+
+// 控制 menu 展示
+const isVisable = ref(false)
+
 
 /**
  * 鼠标移入的触发行为
  */
 const onMouseenter = () => {
-
+  isVisable.value = true
 }
 
 /**
  * 鼠标移出的触发行为
  */
 const onMouseleave = () => {
+  isVisable.value = false
 }
-
 </script>
 
 <style lang="scss" scoped>
