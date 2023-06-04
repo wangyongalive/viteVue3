71c265a395845e45898871fbc71aaef0d8fee0eb
wangyongalive <jsonwang0102@163.com>
2023-06-04 22:41:20 +0800

    confirm组件

---
+++ src/libs/confirm/index.js
@@ -0,0 +1,55 @@
+import { h, render } from 'vue'
+import confirmComponent from './index.vue'
+
+/**
+ *
+ * @param {*} title 标题
+ * @param {*} content 文本
+ * @param {*} cancelText 取消按钮文本
+ * @param {*} confirmText 确定按钮文本
+ * @returns
+ */
+export const confirm = (
+  title,
+  content,
+  cancelText = '取消',
+  confirmText = '确定'
+) => {
+  return new Promise((resolve, reject) => {
+    // 允许只传递 content
+    if (title && !content) {
+      content = title
+      title = ''
+    }
+
+    // 关闭弹层事件
+    const close = () => {
+      // 从 body 上移除组件
+      render(null, document.body)
+    }
+
+    // 取消按钮事件
+    const cancelHandler = () => {
+      reject(new Error('取消按钮点击'))
+    }
+
+    // 确定按钮事件
+    const confirmHandler = () => {
+      resolve()
+    }
+
+    // 1. vnode
+    const vnode = h(confirmComponent, {
+      title,
+      content,
+      cancelText,
+      confirmText,
+      confirmHandler,
+      cancelHandler,
+      close
+    })
+    // 2. render
+    // 使用 render 函数将 vnode 渲染为真实DOM并挂载到 body 上
+    render(vnode, document.body)
+  })
+}
