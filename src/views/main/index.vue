9cded7f7f3d2310c76af92fbd2b616f47fbea405
wangyongalive <jsonwang0102@163.com>
2023-06-23 17:24:54 +0800

    通用组件trigger-menu trigger-menu

--- src/views/main/index.vue
+++ src/views/main/index.vue
@@ -5,10 +5,31 @@
     <div class="max-w-screen-xl mx-auto relative m-1 xl:mt-4">
       <list-vue></list-vue>
     </div>
+    <!-- 移动端下的tabbar -->
+    <!-- fixed居中 m-auto left-0 right-0 w-[220px] -->
+    <m-trigger-menu v-if="isMobileTerminal" class="fixed bottom-6 m-auto left-0 right-0 w-[220px]">
+      <m-trigger-menu-item icon="home" iconClass="fill-zinc-900 dark:fill-zinc-200">
+        首页
+      </m-trigger-menu-item>
+      <m-trigger-menu-item v-if="$store.getters.token" icon="vip" iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500" @click="onVipClick">
+        VIP
+      </m-trigger-menu-item>
+      <m-trigger-menu-item icon="profile" iconClass="fill-zinc-400 dark:fill-zinc-500"
+        textClass="text-zinc-400 dark:text-zinc-500" @click="onMyClick">
+        {{ $store.getters.token ? '我的' : '登录' }}
+      </m-trigger-menu-item>
+    </m-trigger-menu>
   </div>
 </template>
 <script setup>
+import { isMobileTerminal } from '@/utils/flexible'
 import navigationVue from './components/navigation/index.vue'
 import listVue from './components/list/index.vue'
+import { useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+const store = useStore()
+const router = useRouter()
+
 </script>
 <style lang=""></style>
